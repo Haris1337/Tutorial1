73b5f1f8cd607373c6a93d83301b09a116a3c5e0
haris1337 <owxharis@hotmail.com>
2018-01-19 11:20:43 +0100

    added dropdown menu for author and post connected to author

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -13,7 +13,7 @@ class Post(models.Model):
     )
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
-    author = models.CharField(max_length=250) #changed from foregin key User
+    author = models.ForeignKey(User, related_name='blog_posts') #changed from foregin key User
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 from .models import Post
 
 #Prints the 10 first objects in the list
@@ -14,16 +15,23 @@ def post_list(request):
 
 #adds a new object to the list using title, author and body
 def add(request):
+
 	if(request.method == 'POST'):
+		print "request.POST", request.POST
 		title = request.POST['title']
 		author = request.POST['author']
 		body = request.POST['body']
-		
-		post = Post(title=title, author=author, body=body)
+		u = User.objects.get(id=int(author))			
+		post = Post(title=title, author=u, body=body)
 		post.save()
 
 		return redirect('/post')
 	else:
-		return render(request, 'blog/add.html')
+		users = User.objects.all()
+		user = {
+		      'users':users
+		}
+
+		return render(request, 'blog/add.html', user)
 
 
